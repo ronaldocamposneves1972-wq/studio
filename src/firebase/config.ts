15ac6bf8799a9735917b20825af5f49e94c96954
kaique guilherme <kaiqueguilhermepereiramiranda@gmail.com>
2025-10-04 02:47:39 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,6 @@ export const firebaseConfig = {
   "appId": "1:784784341019:web:6eab27c3ff41dd635260be",
   "apiKey": "AIzaSyCFMeomXLvYXki45S_mx2Yy2t3kHx99e-U",
   "authDomain": "studio-4590550537-d6934.firebaseapp.com",
-  "storageBucket": "studio-4590550537-d6934.appspot.com",
   "measurementId": "",
   "messagingSenderId": "784784341019"
 };
