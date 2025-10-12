d97afcac041262398944d6a19d623b7627bf7d4b
kaique guilherme <kaiqueguilhermepereiramiranda@gmail.com>
2025-10-12 21:51:25 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https
+        protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/**',
