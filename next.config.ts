9f7d95b937edd5352a46ffcc082f7fb45c1f87d3
kaique guilherme <kaiqueguilhermepereiramiranda@gmail.com>
2025-10-15 05:10:29 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+  },
   images: {
     remotePatterns: [
       {
